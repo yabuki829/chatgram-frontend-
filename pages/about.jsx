6c37933e46a56d8c916b94c10c414dc57f5611bf
yabuki829 <syodai829@icloud.com>
2024-03-16 17:24:40 +0900

    利用規約を作成

---
+++ pages/about.jsx
@@ -0,0 +1,43 @@
+
+import React from 'react'
+import Link from 'next/link' 
+const about = () => {
+  return (
+    <div className='bg-gray-100 h-screen'>
+        <div className='flex justify-between md:justify-around items-center  bg-white  md:my-10'>
+            <div className='text-center my-10'>
+                <Link className='  text-3xl font-bold' href="/">Chatgram</Link>
+            </div>
+            <div>
+                <a className='mx-2 font-bold' href="">About</a>
+                {/* <Link className=' mx-2 font-bold' href="/terms">利用規約</Link> */}
+                </div>
+            </div>
+        
+        <div className='md:w-2/3 mx-auto bg-white p-4 md:p-10 rounded'>
+           
+            <div className='my-2'>
+                <h1 className='font-bold'>Q1. どんなサイトですか？</h1>
+                <p>A. 匿名でテレビ番組の感想をリアルタイムで投稿できるサービスです。</p>
+            </div>
+            <hr className='py-2' />
+            <div className='my-2'>
+                <h1 className='font-bold'>Q2. 大阪と東京しかありませんが追加の予定はありますか？</h1>
+                <p>A. 今後、利用者の反応を伺いつつ需要がありそうであれば追加する予定です。</p>
+            </div>
+            <hr className='py-2' />
+            <div className='my-2'>
+                <h1 className='font-bold'>Q3. 禁止事項はありますか?</h1>
+                <p>A. 誹謗中傷、個人情報、暴言、その他不適切な投稿は禁止です。詳しくは利用規約を確認ください。</p>
+                 <Link className=' mx-2 font-bold' href="/terms">利用規約</Link>
+            </div>
+            
+        </div>
+
+
+    </div>
+
+  )
+}
+
+export default about
--- pages/index.js
+++ pages/index.js
@@ -1,119 +1,290 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import React, { useState,useEffect } from 'react'
+import axios from "axios";
+import {database } from '../firebase/firebase'
+import { ref, push,onValue } from "firebase/database";
+import { getAuth, onAuthStateChanged, signInAnonymously, } from "firebase/auth";
 
-const inter = Inter({ subsets: ["latin"] });
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+import Link from 'next/link' 
 
-export default function Home() {
+
+const test = () => {
+  const [showContent, setShowContent] = useState(false);
+  const [location,setLocation] =  useState("大阪");
+  const [program, setProgram] = useState({
+    id: "",
+    title: "",
+    detail: "",
+    start_time: "",
+    end_time: "",
+    tv_station: "",
+    room: "d"
+  });
+
+
+  const [userId,setUserId] =  useState("none");
+
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState('');
+
+  const auth = getAuth();
+  
+  useEffect(() => {
+    const dbRef = ref(database, program.room);
+    const unsubscribe = onValue(dbRef, (snapshot) => {
+      const value = snapshot.val();
+      console.log("取得", value);
+      const messages = value ? Object.values(value) : [];
+      setMessages(messages);
+    });
+  
+    return () => unsubscribe();
+  }, [program]);
+
+
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, user => {
+      if (user) {
+        // ユーザーがログインしている
+        console.log("ユーザーがログインしています:", user);
+        setUserId(user.uid)
+        // ここでバンされているアカウントか確認する
+        console.log("バンされているか確認します")
+      } else {
+        // ユーザーがログアウトしている場合、匿名でログインする
+        console.log("ユーザーがログアウトしています。匿名でログインします。");
+
+        signInAnonymously(auth);
+        setUserId(auth.currentUser.uid)
+      }
+    });
+    return () => unsubscribe();
+  }, []);
+  useEffect(() => {
+    setShowContent(false)
+    
+  }, [location]);
+  
+
+  const getRelativeTime = (timestamp) => {
+    const now = new Date();
+    const messageDate = new Date(timestamp);
+    const diffInSeconds = Math.round((now - messageDate) / 1000);
+  
+    if (diffInSeconds < 60) {
+      return `${diffInSeconds}秒前`;
+    } else if (diffInSeconds < 3600) {
+      return `${Math.floor(diffInSeconds / 60)}分前`;
+    } else if (diffInSeconds < 86400) {
+      return `${Math.floor(diffInSeconds / 3600)}時間前`;
+    } else {
+      return `${Math.floor(diffInSeconds / 86400)}日前`;
+    }
+  };
+
+  const formatTime = (time) => {
+    const date = new Date(time);
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    return `${hours}:${minutes}`;
+  };
+  
+  const sendMessage =async () => {
+    const auth = getAuth();
+    const user = auth.currentUser;
+
+    if (user) {
+      try {
+        const dbRef = ref(database,program.room)
+        if (message === ""){
+          return
+        }
+        
+
+        await push(dbRef, {
+          userId: user.uid, 
+          message: message,
+          timestamp: new Date().toISOString(),
+
+        });
+        console.log('Message sent successfully.');
+      } catch (e) {
+        console.error('Error sending message:', e);
+      }
+    } else {
+      console.log('ユーザーがログインしていません。');
+    }
+    setMessage("");
+  };
+
+
+  const handleMessageChange = (event) => {
+      setMessage(event.target.value);
+  };
+
+  const swith_channel = (number) => {
+    setShowContent(true);
+    console.log(location)
+    // alert(location+"の"+number+"チャンネルの番組を取得する")
+    const url = "http://127.0.0.1:8000/api/programs/";
+    const data = {
+      "channel": number,
+      "location":location
+    }
+    axios.get(url,{
+      params: data
+      })
+      .then(response => {
+
+        if (response.data["message"]) {
+         
+          setProgram(
+            {
+              id: "",
+              title: "このチャンネルは現在未対応です",
+              detail: "",
+              start_time: "",
+              end_time: "",
+              tv_station: "",
+              room: "d"
+            }
+            
+          )
+        }
+        else {
+          console.log("データ",response.data);
+          setProgram(response.data)
+        }
+        
+        
+      })
+      .catch(error => {
+        console.error('番組情報の取得中にエラーが発生しました', error);
+        // エラーハンドリングを行う
+      });
+    
+  };
+  const handleChange = (e) => {
+    setLocation(e.target.value)
+  }
+  
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className='bg-gray-100 h-screen'>
+      <div className='flex justify-between md:justify-around items-center  bg-white'>
+       <div className='text-center my-10'>
+       <h1 className='  text-3xl font-bold'>Chatgram</h1>
+        <select className='bg-white border' onChange={(e) => handleChange(e)}>
+          <option value="大阪">大阪</option>
+          <option value="東京">東京</option>
+              
+        </select>
+       </div>
+       <div>
+                <Link className='mx-2 font-bold' href="/about">About</Link>
+                {/* <Link className=' mx-2 font-bold' href="/terms">利用規約</Link> */}
+            </div>
+
+      </div>
+     
+
+      {
+        showContent ? (
+          <div className='md:w-2/3 mx-auto bg-white p-4 md:p-10 rounded md:my-10'>
+           
+
+           
+            <div className='mx-2'>
+              <div className='flex  items-center'>
+                <button className='border-2 rounded-full w-8 h-8 md:w-12 md:h-12 flex items-center justify-center my-5 mr-2 flex-shrink-0' onClick={() => setShowContent(false)} >←</button>
+                <h1 className='md:text-left font-bold text-xl md:text-3xl '>{program.title}</h1>
+              </div>
+              {program.id !== "" && (
+              <p className=''>放送時間: {formatTime(program.start_time)} ~ {formatTime(program.end_time)}</p>
+              )}
+            </div>
+            
+            <div>
+
+           
+            <div>
+             
+              {messages.map((msg, index) => (
+                <div key={index} className='my-2'>
+                  <hr />
+                  {
+                    msg.userId === userId ?
+                    <>
+                      <h1>{index + 1} : {msg.userId}</h1>
+                      <div className='flex justify-end'>
+                        <p className='bg-blue-500 inline-block px-3 py-2 text-white rounded-full my-2 text'>{msg.message}</p> 
+                      </div>
+                      
+                      <p className='text-gray-500'>{getRelativeTime(msg.timestamp)}</p>
+                    </> :
+                    <>
+                      <h1>{index + 1} : {msg.userId}</h1>
+                      <p className='bg-green-300 inline-block px-2 py-1 rounded-full my-2'>{msg.message}</p> 
+                      <p className='text-gray-500'>{getRelativeTime(msg.timestamp)}</p>
+                    </>
+                  }
+                 
+              
+                </div>
+                
+                
+              ))}
+            </div>
+        
+            
+            
+           
+            <hr />
+            <br />
+            {program.id !== "" && (
+            <div>
+              <h1><strong>誹謗中傷</strong>、<strong>個人情報</strong>、<strong>暴言</strong>、 <strong>その他不適切な投稿</strong>は禁止です。</h1>
+               <Link className='text-blue-500 mx-2 font-bold' href="/terms">利用規約</Link>
+              <div className='flex'>
+                <textarea className='w-full border p-2' type="text" value={message} onChange={handleMessageChange}  placeholder="メッセージを入力" />
+                <button className='pl-1' onClick={sendMessage}>
+                  <FontAwesomeIcon style={{ fontSize: "24px", color: "blue" }} icon={faPaperPlane} />
+                </button>
+              </div>
+            </div>
+            )}
+           
         </div>
-      </div>
+            
+        </div>
+        ):(
+          <div className='md:w-1/3 mx-auto'>
+              
+            <div className='grid grid-cols-3'>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+              {[...Array(12).keys()].map((number) => (
+                <div key={number} className='flex justify-center my-2 '>
+                  <button
+                  className='border-2 border-black bg-white hover:bg-black hover:text-white rounded-full w-24 md:w-32 h-24 md:h-32 text-3xl'
+                  onClick={() => swith_channel(number + 1)} 
+                >
+                  {number + 1}
+                </button>
+                </div>
+              ))}
+            </div>
+          </div>
+       
+    
+        )
+      }
+      
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+      
+     
+    </div>
+  )
 }
+
+export default test
